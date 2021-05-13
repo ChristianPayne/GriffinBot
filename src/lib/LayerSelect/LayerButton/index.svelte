732f007dd9dffbc92108bdf88626589f37e4cdec
Christian Payne <christian.p@modelmatch.com>
2021-05-12 20:31:28 -0700

    Working on Layer Select page and buttons.

---
+++ src/lib/LayerSelect/LayerButton/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let layerName;
+
+  function onClick () {
+    localStorage.setItem("LayerName", layerName);
+  }
+</script>
+
+<button on:click={onClick}>{layerName}</button>
+
+<style>
+  button {
+    background-color: var(--accent-color);
+    padding: 5%;
+    margin: 1em;
+    word-wrap: normal;
+    min-width: 10em;
+    max-width: 20em;
+    border-radius: 0.5em;
+  }
+</style>
---
+++ src/lib/LayerSelect/index.svelte
@@ -0,0 +1,28 @@
+<script>
+// localStorage.setItem("Testing","This is the value.");
+// localStorage.clear();
+import LayerButton from "./LayerButton/index.svelte";
+</script>
+
+<section>
+  <h1>Layer Select</h1>
+  <div class="flex">
+    <LayerButton layerName="Page 1"></LayerButton>
+    <LayerButton layerName="Page 2"></LayerButton>
+    <LayerButton layerName="Page 3"></LayerButton>
+    <LayerButton layerName="Page 4"></LayerButton>
+    <LayerButton layerName="Page 5"></LayerButton>
+    <LayerButton layerName="Page 6"></LayerButton>
+  </div>
+  <button>Clear localStorage</button>
+</section>
+
+<style>
+  .flex {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+    background-color: var(--heading-color);
+  }
+</style>
