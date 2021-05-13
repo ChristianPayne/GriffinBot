eeaa5ca73a6dd5edb9d0bef3a5c5e9a967c84e3c
Christian Payne <christian.p@modelmatch.com>
2021-05-12 23:56:02 -0700

    Fixed error of server trying to access localStorage.

--- src/lib/GriffinBotStore.ts
+++ src/lib/GriffinBotStore.ts
@@ -4,16 +4,19 @@ export const activeLayer = writable("");
 
 // Get from local storage.
 export function setActiveLayer (layerName: string) {
+  if(typeof(window) === 'undefined') return;
   activeLayer.update(() => layerName);
   localStorage.setItem("LayerName", layerName);
 }
 
 export async function getActiveLayer () {
+  if(typeof(window) === 'undefined') return;
   const layerName = await localStorage.getItem("LayerName");
   setActiveLayer(layerName);
 }
 
 export function clearActiveLayer () {
+  if(typeof(window) === 'undefined') return;
   activeLayer.update(()=>"")
   localStorage.setItem("LayerName", "");
 }
