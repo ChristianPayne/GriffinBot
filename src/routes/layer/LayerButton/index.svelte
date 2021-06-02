c478b24c26db437ea76ba25e55641a863a50cb65
Christian Payne <christianpayne522@gmail.com>
2021-06-01 20:24:19 -0700

    Scrapping everything and switching to react/netlify

--- src/routes/layer/LayerButton/index.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import { navigateToPath } from "$lib/GriffinBotStore";
-  export let pathName;
-  export let path;
-</script>
-
-<button on:click={()=>{navigateToPath(path)}}>{pathName}</button>
