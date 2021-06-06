e3ca2d8f3805d287f6a4d15a065a493235a1d468
Christian Payne <christianpayne522@gmail.com>
2021-06-06 12:48:35 -0700

    Added web server.

--- bot.js
+++ bot.js
@@ -1,8 +1,20 @@
-require("dotenv").config();
+function loadServer () {
+  const HTTP = require('http');
+  const PORT = process.env.PORT ?? 0522;
 
+  console.log(`Env port: ${process.env.PORT}. Port value: ${PORT}`);
 
+  HTTP.createServer((request, response) => {
+       response.writeHead(200, {'Content-Type': 'text/plain'});
+       response.write(`Look at my website!`);
+       response.end();
+  }).listen(PORT);
+  console.log(`Server was made on port: ${PORT}`);
+}
+loadServer();
 
 var ComfyJS = require("comfy.js");
+const { load } = require("dotenv");
 ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
   if( command === "test" ) {
     ComfyJS.Say( "replying to !test" );
