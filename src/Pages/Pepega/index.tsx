235287f1e4d3b07d4eedd633695d72502d7be32d
NadirVaknin <nadirv37@gmail.com>
2021-08-22 21:26:51 +0300

    New landing page design

--- src/Pages/Pepega/index.tsx
+++ src/Pages/Pepega/index.tsx
@@ -4,9 +4,21 @@ import './style.css';
 const Pepega = () => {
     return (
         <div>
-            <h1>This is a pepega page! </h1>
-            <p>GriffinBot - the bot that will make your friends jelous, and the girls dripping, sign up today!</p>
-            <button className = 'signUp'>Sign Up!</button>
+            <body className = 'backGround'>
+            <h1><a href="#">GriffinBot</a></h1>
+
+            <ul>
+            <li className = 'Login'><a href="#">Login</a></li>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">Features</a></li>
+            </ul> 
+            <h2>GriffinBot - the all around bot for your twitch channel!</h2>
+            <p>GriffinBot is an all around twitch bot that will make you stream legnedery!<br></br>
+                The bot offers a bunch of amazing features such as moderating, sound commands, custom commands and more!<br></br>
+                Hit the button down below to connect your twitch account now!
+            </p>
+            <button className = 'twitchButton'>Connect With Twitch</button>
+            </body>
         </div>
     );
 };
